c972de516d6b04935ef9da713c23ef0644a2b174
harshita9708 <harshitahosamani04@gmail.com>
2025-04-23 09:19:15 +0530

    sign up

---
+++ App.java
@@ -0,0 +1,8 @@
+class App{
+    public static void main(String[] args) {
+        String name = "Java";
+        if (name.equals("Java")) {  // Use .equals() to compare string content
+            System.out.println("The computer language is " + name);
+        }
+    }
+}
