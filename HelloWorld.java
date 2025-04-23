e0479074d1c7a29498a829834718755a65eb2789
harshita9708 <harshitahosamani04@gmail.com>
2025-04-23 09:32:54 +0530

    save

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This is the main method, which is the entry point of any Java program
+        System.out.println("Hello, World!");  // This prints the text to the console
+    }
+}
+ 
+    
+
